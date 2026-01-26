2cd343e7d24de982ce46c55c73fd9d526421bed0
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-01-26 22:19:42 +0530

    Add multiplication table functionality

--- Table.java
+++ Table.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+import java.util.Scanner;
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(num+"x"+i+"="+(num * i));
+        }
+
+    }
+    
+}
