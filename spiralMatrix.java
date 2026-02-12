efe8a889b677e88851f4d28bd0c8e87981b5e149
rahulhattinagre <rahulhattinagre@gmail.com>
2026-02-12 22:21:56 +0530

    resolved merge conflict

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -1,44 +1,49 @@
-public class spiralMatrix{
-    public static void spiralMatrix(int matrix[][]){
-        int startRow=0;
-        int startCols=0;
-        int endRow=matrix.length-1;
-        int endCols=matrix[0].length-1;
+public class spiralMatrix {
+    public static void spiralPrint(int matrix[][]) {
+        int startRow = 0;
+        int startCols = 0;
+        int endRow = matrix.length - 1;
+        int endCols = matrix[0].length - 1;
 
-        while(startRow<=endRow && startCols<=endCols){
-            //top
-            for(int j=startCols;j<=endCols;j++){
-                System.out.print(matrix[startRow][j]+"");
+        while (startRow <= endRow && startCols <= endCols) {
+
+            // top
+            for (int j = startCols; j <= endCols; j++) {
+                System.out.print(matrix[startRow][j] + " ");
             }
-            //right
-            for(int i=startRow+1;i<=endRow;i++){
-                System.out.print(matrix[i][endCols]+"");
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCols] + " ");
             }
-            //bottom
-            for(int j=endCols-1;j>=startCols;j--){
-                if(startRow==endRow){
-                    break;
-                }
-                System.out.print(matrix[endRow][j]+"");
+
+            // bottom
+            for (int j = endCols - 1; j >= startCols; j--) {
+                if (startRow == endRow) break;
+                System.out.print(matrix[endRow][j] + " ");
             }
-            //left
-            for(int i=endRow-1;i>=startRow+1;i--){
-                if(startCols==endCols){
-                    break;
-                }
-                System.out.print(matrix[i][startCols]+"");
+
+            // left
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                if (startCols == endCols) break;
+                System.out.print(matrix[i][startCols] + " ");
             }
+
             startCols++;
             startRow++;
             endCols--;
             endRow--;
         }
     }
+
     public static void main(String[] args) {
-        int matrix[][]={{1,2,3,4},
-                        {5,6,7,8},
-                        {9,10,11,12},
-                        {13,14,15,16}};
-        spiralMatrix(matrix);
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+
+        spiralPrint(matrix);
     }
 }
